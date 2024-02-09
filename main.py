1cc2a5e0d2840d76e623a94b18761ff1ff9a4240
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-02-09 12:56:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ st.title("""TUMCHANGIE KIJANA - FUNDRAISER """)
 
 st.subheader("A young man's future on hold due to exam fees. Help him reach his potential by contributing towards his tuition. Every bit counts, let's unlock his dreams together! Donate now. ")
 col1, col2 = st.columns([3,3])
-with col1:
-     col1.expander("click to read more")
-     col1.markdown("Use the control number below to make your contribution, tumia control number kutoka kwenye picha chini kumchangia kijana thank you")
-     col1.image("124.PNG")
-with col2:
-     col2.expander("Picha")
-     col2.image("john.jpeg")
+with col1.expander(""click to read more"):
+     st.markdown("Use the control number below to make your contribution, tumia control number kutoka kwenye picha chini kumchangia kijana thank you")
+     st.image("124.PNG")
+with col2.expander("Picha"):
+     st.image("john.jpeg")
     
 
 st.file_uploader("Upload receipt")

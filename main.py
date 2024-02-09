87d0bee0b8f3d25c68c624784e99ad2cee8b13d8
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-02-09 12:54:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import time
 st.set_page_config(page_title="TUMCHANGIE KIJANA", page_icon=":wave:")
 st.title("""TUMCHANGIE KIJANA - FUNDRAISER """)
 
-col1, col2 = st.columns([3,3])
-
 st.subheader("A young man's future on hold due to exam fees. Help him reach his potential by contributing towards his tuition. Every bit counts, let's unlock his dreams together! Donate now. ")
-
+col1, col2 = st.columns([3,3])
 with col1:
-     st.expander("click to read more")
-     st.markdown("Use the control number below to make your contribution, tumia control number kutoka kwenye picha chini kumchangia kijana thank you")
-     st.image("124.PNG")
+     col1.expander("click to read more")
+     col1.markdown("Use the control number below to make your contribution, tumia control number kutoka kwenye picha chini kumchangia kijana thank you")
+     col1.image("124.PNG")
 with col2:
-     st.expander("Picha")
-     st.image("john.jpeg")
+     col2.expander("Picha")
+     col2.image("john.jpeg")
     
 
 st.file_uploader("Upload receipt")
